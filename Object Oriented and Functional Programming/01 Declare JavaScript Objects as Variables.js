29dd46f06a383cceded264343a0ccc7cd54d6073
Crossb <crosskiev@gmail.com>
2017-06-09 23:37:13 +0300

    2

--- Object Oriented and Functional Programming/01 Declare JavaScript Objects as Variables.js
+++ Object Oriented and Functional Programming/01 Declare JavaScript Objects as Variables.js
@@ -19,7 +19,20 @@ _____________Instruction
  var motorBike = {
 
  // Only change code below this line.
+ var car = {
+ "wheels":4,
+ "engines":1,
+ "seats":5
+ };
 
+ var motorBike = {
+
+ // Only change code below this line.
+ 
+ "wheels": 2,
+ "engines":1,
+ "seats":2
+ };
  };
 
 
