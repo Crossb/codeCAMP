385f50fe5a69e08fc9190a6dffe50c4fd3bb84a1
Crossb <crosskiev@gmail.com>
2017-06-11 23:10:20 +0300

    4th

---
+++ Basic JavaScript/07 Add Two Numbers with JavaScript.js
@@ -0,0 +1,20 @@
+
+_____________Instruction
+
+/*
+ Subtract One Number from Another with JavaScript
+ We can also subtract one number from another.
+
+ JavaScript uses the - symbol for subtraction.
+
+ Example
+
+ myVar = 12 - 6; // assigned 6
+ Instructions
+ Change the 0 so the difference is 12.
+
+ */
+
+  ____________Execution
+
+var difference = 45 - 33;
---
+++ Basic JavaScript/08 Multiply Two Numbers with JavaScript .js
@@ -0,0 +1,22 @@
+
+_____________Instruction
+
+/*
+ Multiply Two Numbers with JavaScript
+ We can also multiply one number by another.
+
+ JavaScript uses the * symbol for multiplication of two numbers.
+
+ Example
+
+ myVar = 13 * 13; // assigned 169
+ Instructions
+ Change the 0 so that product will equal 80.
+
+ Make the variable product equal 80
+ Use the * operator
+ */
+
+  ____________Execution
+
+var product = 8 * 10;
