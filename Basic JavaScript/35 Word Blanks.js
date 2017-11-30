45dd782fd77ad7cc111151b2d522b4f9fd582ba5
Yaroslav <crosskiev@gmail.com>
2017-11-30 13:41:28 +0200

    add new chalenges in basic part

---
+++ Basic JavaScript/35 Word Blanks.js
@@ -0,0 +1,28 @@
+
+// _____________Instruction
+
+/*
+	Store Multiple Values in one Variable using JavaScript Arrays 
+
+
+With JavaScript array variables, we can store several pieces of data in one place.
+
+You start an array declaration with an opening square bracket, end it with a closing square bracket, and put a comma between each entry, like this:
+
+var sandwich = ["peanut butter", "jelly", "bread"].
+
+Instructions
+Modify the new array myArray so that it contains both a string and a number (in that order).
+
+Hint
+Refer to the example code in the text editor if you get stuck.
+ */
+
+  // ____________Execution
+
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Yaroslav", 38];
---
+++ Basic JavaScript/37 Nest one Array within Another Array .js
@@ -0,0 +1,20 @@
+
+// _____________Instruction
+
+/*
+ Nest one Array within Another Array 
+You can also nest arrays within other arrays, like this: [["Bulls", 23], ["White Sox", 45]]. This is also called a Multi-dimensional Array.
+
+Instructions
+Create a nested array called myArray.
+ */
+
+  // ____________Execution
+
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["age", 38], ["a was year born", 1979]];
+
